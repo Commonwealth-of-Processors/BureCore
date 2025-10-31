1cc46fbab53a33744843faee78181be3798e2695
Cra2yPierr0t <Cra2yPierr0t@vlsi.jp>
2025-11-01 01:27:48 +0900

    add id to core

--- rtl/BC_core.sv
+++ rtl/BC_core.sv
@@ -3,6 +3,8 @@ module BC_core #(
   parameter DATA_WIDTH  = 32,
   parameter DATA_WIDTH  = 32
 )(
+  input  logic i_clk,
+  input  logic i_rstn,
   CG_memory_interface.to_memory if_dmem,
   CG_memory_interface.to_memory if_imem
 );
@@ -17,9 +19,28 @@ module BC_core #(
     .ADDR_WIDTH   (ADDR_WIDTH   ),
     .INSTR_WIDTH  (INSTR_WIDTH  )
   ) instruction_fetch (
+    .i_clk          (),
+    .i_rstn         (),
     .if_imem        (if_imem        ),
+
+    .i_new_pc       (),
+    .i_prst         (),
+
     .o_instr_valid  (w_instr_valid  ),
     .o_instr        (w_instr        )
+  );
+
+  BC_stage_id #(
+    .DATA_WIDTH   (DATA_WIDTH   ),
+    .INSTR_WIDTH  (INSTR_WIDTH  )
+  ) instruction_decode (
+    .i_clk          (),
+    .i_rstn         (),
+
+    .i_instr_valid  (w_instr_valid  ),
+    .i_instr        (w_instr        ),
+
+    o_decode_valid  (),
   );
 
 endmodule
